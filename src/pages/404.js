facfcbee50344b9bb0be4dac414b9af987b02902
zenio <zeniocode@gmail.com>
2019-05-16 17:44:56 +0500

    Custom 404 page added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
-import React from "react"
+import React from "react";
+import Img from "gatsby-image";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = ({ data }) => {
+	console.log(data.placeholderImage.childImageSharp.fluid);
+	return (
+		<Layout>
+			<SEO title="404: Not found" />
+			<h1>NOT FOUND</h1>
+			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<Img fluid={data.placeholderImage.childImageSharp.fluid} />
+		</Layout>
+	);
+};
 
-export default NotFoundPage
+export const query = graphql`
+	query {
+		placeholderImage: file(relativePath: { eq: "404.png" }) {
+			childImageSharp {
+				fluid(maxWidth: 960) {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+	}
+`;
+
+export default NotFoundPage;
